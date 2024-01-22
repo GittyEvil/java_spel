185da9bd166e8b7475231bb5c18bce210679735d
Evilll <adrian.stude@gmail.com>
2024-01-22 09:23:55 +0100

    s

---
+++ Spel/src/Grafik.java
@@ -0,0 +1,29 @@
+//importerar allt från swing me *
+import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+
+public class Grafik extends JFrame {
+
+    private JButton Button;
+    private JLabel text;
+
+    //constructor
+    public Grafik() {
+        this.setLayout(new GridLayout(2,3));
+        text = new JLabel("hej på dig");
+        Button = new JButton("Ta på mig");
+        this.add(Button);
+        this.add(text);
+        this.setSize(1920, 1080);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+
+
+
+
+
+}
