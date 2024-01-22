803ce1d3f0f36be5a9d5b1d7d3e85bcc05f433cd
Evilll <adrian.stude@gmail.com>
2024-01-22 11:20:18 +0100

    hej

--- Spel/src/Spel/Game.java
+++ Spel/src/Spel/Game.java
@@ -4,17 +4,31 @@ import java.awt.Graphics2D;
 import java.util.*;
 
 public class Game {
+	private Color current;
+	private int timer;
+	private BlueBox rekt;
 	public Game(GameBoard board) {
-
+		current = Color.WHITE;
+		this.timer = 0;
+		rekt = new BlueBox(20,20,20,20);
+		
 	}
 
 	public void update(Keyboard keyboard) {
-
+		this.timer++;
+		if(this.timer % 40 == 0) {
+			
+		}
 	}
 
 	public void draw(Graphics2D graphics) {
-		graphics.setColor(Color.WHITE);
+		/*
+		 * graphics.setColor(current);
 		graphics.drawString("Halå", 100, 100);
 		graphics.fillRect(100, 100, 50, 50);
+		 * */
+		
+		rekt.draw(graphics);
+		
 	}
 }
